cc5ba459e115572a58a6cf8f26dcc0e48ccbd1ee
Florent TORNICELLI <florent@geode.com>
2023-03-24 12:35:59 +0100

    style: remove unused code

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,22 +2,6 @@
 
 namespace App\Controller;
 
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\Routing\Annotation\Route;
-
-// class ChatController extends AbstractController
-// {
-//     #[Route('/chat', name: 'app_chat')]
-//     public function index(): Response
-//     {
-        
-
-//         return $this->render('chat/index.html.twig', [
-//             'controller_name' => 'ChatController',
-
-
-
 use App\DTO\Chat\CreateChatDto;
 use App\DTO\Chat\JoinChatDto;
 use App\Form\Chat\CreateChatType;
